89698af8b6366dca912fdb1f39e38760da61ea21
Doug Ellwanger <doug.ellwanger@gmail.com>
2012-02-26 16:02:08 -0800

    Allow alt-tab.

--- user_input.py
+++ user_input.py
@@ -5,17 +5,28 @@ dtheta = 0
 dphi = 0
 forward = 0
 sideways = 0
+process_input = True
 
 def update(events):
-    global escape, dtheta, dphi, forward, sideways
+    global escape, dtheta, dphi, forward, sideways, process_input
     """Returns dtheta, dphi, dforward"""
     dtheta = 0
     dphi = 0
     for event in events:
         if event.type == pygame.QUIT:
             escape = True
+        elif event.type == pygame.ACTIVEEVENT:
+            if event.gain:
+                pygame.event.set_grab(True)
+                process_input = True
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
+            if not process_input:
+                continue
+            elif event.key == pygame.K_TAB:
+                if pygame.key.get_mods() & (pygame.KMOD_LALT | pygame.KMOD_RALT):
+                    pygame.event.set_grab(False)
+                    process_input = False
+            elif event.key == pygame.K_ESCAPE:
                 escape = True
             elif event.key == pygame.K_w:
                 forward = 1
@@ -35,4 +46,5 @@ def update(events):
             if event.key == pygame.K_a and sideways == -1:
                 sideways = 0
     # do mouse stuff
-    dtheta, dphi = pygame.mouse.get_rel()
+    if process_input:
+        dtheta, dphi = pygame.mouse.get_rel()
